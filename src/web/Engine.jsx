ef015205f352e83579c4eaa972fcd0f8e802493e
sunkeysun <sunkeysun@tencent.com>
2019-01-10 20:06:40 +0800

    引擎面向对象化

---
+++ src/web/Engine.jsx
@@ -0,0 +1,64 @@
+/**
+ * web 端视图引擎
+ *
+ * @author: sunkeysun
+ */
+
+import React from 'react'
+import ReactDom from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+import Root from './Root'
+
+export default class Engine {
+    _appEntryTpl = `
+        import Kernel from 'inventor/web'
+        import ViewEngine from 'inventor-view-react-redux/web'
+        import App from '<-appPath->/App'
+        import reducers from '<-appPath->/redux'
+        import appConfig from '<-webPath->/config/app'
+
+        const kernel = new Kernel({ appConfig, App, reducers })
+        const engine = ViewEngine()
+        kernel.run(() => {
+            engine.render({ App, reducers })
+        })
+    `
+
+    render({ App, reducers }) {
+        const initialState = window.__INITIAL_STATE__
+        const ssr = window.__SSR__
+        const appName = window.__APP_NAME__
+        const rootDomId = window.__ROOT_DOM_ID__
+        const constants = window.__CONSTANTS__
+
+        const history = createHistory()
+
+        const rootReducer = combineReducers({
+            ...reducers,
+            constants: (state=constants) => state,
+            routing: routerReducer,
+        })
+
+        const store = createStore(rootReducer, initialState)
+
+        let render = ReactDom.render
+        if (ssr) {
+            render = ReactDom.hydrate
+        }
+
+        const rootState = { store, App, history }
+
+        render(
+            <Root { ...rootState } />,
+            document.getElementById(rootDomId)
+        )
+    }
+
+    getAppEntry({ appPath, webPath }) {
+        const tplContent = appEntryTpl.replace(/<-appPath->/g, appPath)
+                                      .replace(/<-webPath->/g, webPath)
+        return tplContent
+    }
+}
--- src/web/index.js
+++ src/web/index.js
@@ -1,63 +1,7 @@
 /**
- * web 渲染器
+ * web 端视图引擎入口
  *
  * @author: sunkeysun
  */
-import React from 'react'
-import ReactDom from 'react-dom'
-import { createStore, combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
-import createHistory from 'history/createBrowserHistory'
-import Root from './Root'
 
-const appEntryTpl = `
-    import Kernel from 'inventor/web'
-    import viewEngine from 'inventor-view-react-redux/web'
-    import App from '<-appPath->/App'
-    import reducers from '<-appPath->/redux'
-    import appConfig from '<-webPath->/config/app'
-
-    const kernel = new Kernel({ appConfig, App, reducers })
-    kernel.run(() => {
-        viewEngine().render({ App, reducers })
-    })
-`
-
-export default function engine() {
-    return {
-        render({ App, reducers }) {
-            const initialState = window.__INITIAL_STATE__
-            const ssr = window.__SSR__
-            const appName = window.__APP_NAME__
-            const rootDomId = window.__ROOT_DOM_ID__
-            const constants = window.__CONSTANTS__
-
-            const history = createHistory()
-
-            const rootReducer = combineReducers({
-                ...reducers,
-                constants: (state=constants) => state,
-                routing: routerReducer,
-            })
-
-            const store = createStore(rootReducer, initialState)
-
-            let render = ReactDom.render
-            if (ssr) {
-                render = ReactDom.hydrate
-            }
-
-            const rootState = { store, App, history }
-
-            render(
-                <Root { ...rootState } />,
-                document.getElementById(rootDomId)
-            )
-        },
-        getAppEntry({ appPath, webPath }) {
-            const tplContent = appEntryTpl.replace(/<-appPath->/g, appPath)
-                                          .replace(/<-webPath->/g, webPath)
-            return tplContent
-        },
-    }
-}
+export default from './Engine'
