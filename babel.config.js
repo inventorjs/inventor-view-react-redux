d6faefcc2583dbb1d65320e600e5597133c6b594
sunkeysun <sunkeysun@tencent.com>
2022-03-03 21:40:39 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = function(api) {
         plugins: [
             ['@babel/plugin-transform-runtime'],
             ['@babel/plugin-proposal-decorators', { legacy: true }],
-            ['@babel/plugin-proposal-class-properties', { loose: true }],
+            ['@babel/plugin-proposal-class-properties'],
             '@babel/plugin-proposal-export-default-from',
             '@babel/plugin-proposal-export-namespace-from',
         ]
